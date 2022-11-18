0c5742ca595cfcff921735c0c74267cce8f04648
Bruno Leandro <bruno.lbs100@gmail.com>
2022-11-18 16:35:11 -0300

    'ajustes'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Header from './components/Header/Header'
 import Formulario from './components/Formulario/Formulario'
+import Home from './components/Home/Home'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App = () => {
+
     return (
         <>
         <Header/>
-        <Formulario/>
+        <Router>
+            <Routes>
+                <Route exact path='/form' element={ <Formulario/>}/>
+                <Route exact path='/' element={<Home/>} />
+            </Routes>
+        </Router>
         </>
     )
 }
